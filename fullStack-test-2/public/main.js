8b732a4083c7899ff6116bf5ca7648f2fed87a27
test <test@MacBook-Pro-van-test.local>
2020-07-28 11:34:31 +0200

    1.1.1

--- fullStack-test-2/public/main.js
+++ fullStack-test-2/public/main.js
@@ -3,13 +3,19 @@ const update = document.querySelector('#update-button');
 update.addEventListener('click', _ => {
     //send put request here
     fetch('quotes', {
-        method: 'put',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            name: 'Dark Vadar',
-            quote: 'I find your lack of faith disturbing.'
+            method: 'put',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: 'Dark Vadar',
+                quote: 'I find your lack of faith disturbing.'
+            })
         })
-    })
+        .then(res => {
+            if (res.ok) return res.json()
+        })
+        .then(response => {
+            console.log(response);
+        })
 })
